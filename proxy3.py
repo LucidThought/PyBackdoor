8158186ec5f487e4962fc83b2a4a7dac26d423d3
LucidThought <alata.it@gmail.com>
2017-02-12 14:16:41 -0700

    -hex flag works as expected with code taken from referenced website

--- proxy3.py
+++ proxy3.py
@@ -14,6 +14,7 @@ import getopt
 import sys
 import string
 from threading import Thread
+from functools import reduce
 
 PROXY_PORT = 0
 DST_PORT = 0
@@ -45,7 +46,7 @@ def main():
       PROXY_PORT = int(sys.argv[2])
       DST_HOST = str(sys.argv[3])
       DST_PORT  = int(sys.argv[4])
-      LOG_MODE = 1  #hard coding raw here regardless of what -arg is entered
+#      LOG_MODE = 1  #hard coding raw here regardless of what -arg is entered
     
     if len(sys.argv) == 4:
       PROXY_PORT = int(sys.argv[1])
@@ -63,18 +64,20 @@ def main():
 # Description:
 # helper function to set the proxy log mode.
 def set_log_mode(LOG_COMMAND):
+  global LOG_MODE
   if LOG_COMMAND == "-raw":
     mode = 1
     print("-raw mode logging enabled")
-  if LOG_COMMAND == "-strip":
+  elif LOG_COMMAND == "-strip":
     mode = 2
     print("-strip mode logging enabled")
-  if LOG_COMMAND == "-hex":
+  elif LOG_COMMAND == "-hex":
     mode = 3
     print("-hex mode logging enabled")
-  if LOG_COMMAND == "-autoN":
+  elif LOG_COMMAND == "-autoN":
     mode = 4
     print("-autoN mode logging enabled")
+  LOG_MODE = mode	
 
 # void start_proxy_server( no args )
 # Description:
@@ -157,20 +160,35 @@ def log_request(data,mode):
       # the other modes? https://docs.python.org/3/howto/unicode.html very useful page
       # only if your converting from UNI Code (byte string) to string
 #    print( symbol + str(data,'utf-8',"ignore"))
-    symbol_format = str.replace(str(data,'utf-8',"ignore"), "\r\n", ("\n\r{}".format(symbol)))
+    symbol_format = str.replace(str(data,'utf-8',"ignore"), "\n", ("\n{}".format(symbol)))
     print(symbol + symbol_format)
 
   elif LOG_MODE == 2:
 #    print("Port logger -strip mode not implemented")
-    symbol_format = "".join(c for c in str(data,"utf-8","ignore") if c.isprintable()) # THIS DOESN'T WORK only strips non-prinatble characters
+    symbol_format = str.replace(str(data,'utf-8',"ignore"), "\n", ("\n{}".format(symbol)))
+#    for c, r in {'':'.',}.items(): INCOMPLETE
     print(symbol_format)
     # Not implemented yet
     # UPDATE: See http://web.itu.edu.tr/sgunduz/courses/mikroisl/ascii.html for details on non-printable characters
+    # FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
 
   elif LOG_MODE == 3:
-    print("Port logger -hex mode not implemented")
+    # print("Port logger -hex mode not implemented")
     # Not Implemented yet
     # this is going to be hard. you can run "cat proxy3.py | hexdump -C" to see what it looks like
+#    separate = '.'
+#    length = 16
+#    counter = 0
+#    for i in xrange(0, len(data), length):
+#      d,data = data[:length],data[length:]
+#      hexval = ' '.join(["%02X"%ord(chr(x)) for x in d])
+#      d = d.translate(FILTER)
+#      output.append("%04X   %-*s   %s\n"%(counter,length*3,hexval,d))
+#    print(output)
+
+    symbol_format = str(data, 'utf-8', "ignore")
+#    print(reduce(lambda x,y:x+y+" ", map(lambda p:("0" if ord(p)<=0xf else "")+hex(ord(p))[2:],symbol_format), ""))
+    print(hexdump(symbol_format))
 
   elif LOG_MODE == 4:
     print("Port logger -AutoN mode not implemented")
@@ -180,6 +198,42 @@ def log_request(data,mode):
     ## In the below example we'll have to fill empty space at the end of the data string with empty spaces
     # loop (while there is still data to print)
     #   loop (while counter < N)
+
+# The following function was taken from https://gist.github.com/ImmortalPC/c340564823f283fe530b
+def hexdump(src, length=16, sep='.'):
+  result = [];
+  # Python3 support
+  try:
+    xrange(0,1);
+  except NameError:
+    xrange = range;
+
+  for i in xrange(0, len(src), length):
+    subSrc = src[i:i+length];
+    hexa = '';
+    isMiddle = False;
+    for h in xrange(0,len(subSrc)):
+      if h == length/2:
+        hexa += ' ';
+      h = subSrc[h];
+      if not isinstance(h, int):
+        h = ord(h);
+      h = hex(h).replace('0x','');
+      if len(h) == 1:
+        h = '0'+h;
+      hexa += h+' ';
+    hexa = hexa.strip(' ');
+    text = '';
+    for c in subSrc:
+      if not isinstance(c, int):
+        c = ord(c);
+      if 0x20 <= c < 0x7F:
+        text += chr(c);
+      else:
+        text += sep;
+    result.append(('%08X:  %-'+str(length*(2+1)+1)+'s  |%s|') % (i, hexa, text));
+
+  return '\n'.join(result);
 
 # client_connect( arg1=socket), returns byte String (Unicode)
 # Description:
@@ -207,7 +261,7 @@ def get_data(socket):
 if __name__ == "__main__":
   main()
 
-  
+
 
 
 
